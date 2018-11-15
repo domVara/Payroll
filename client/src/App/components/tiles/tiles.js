f15209d655372735aa564032f7c1b6555a679465
Dominic <dominic.varakukala@gmail.com>
2018-11-14 23:47:53 -0800

    almost done with navbar and tiles

--- client/src/App/components/tiles/tiles.js
+++ client/src/App/components/tiles/tiles.js
@@ -12,18 +12,35 @@ class FrontPage extends Component {
       <div>
         <div className = "columns columnsRow">
           <div className = "boxPadding">
-            <div className = "box boxSize">
-              asd
+            <div className = "box boxSize has-text-centered">
+              Employees
             </div>
           </div>
           <div className = "boxPadding">
-            <div className = "box boxSize">
-              asd
+            <div className = "box boxSize has-text-centered">
+              Add New Employee
             </div>
           </div>
           <div className = "boxPadding">
-            <div className = "box boxSize">
-              asd
+            <div className = "box boxSize has-text-centered">
+              Teams
+            </div>
+          </div>
+        </div>
+        <div className = "columns columnsRow">
+          <div className = "boxPadding">
+            <div className = "box boxSize has-text-centered">
+              General Records
+            </div>
+          </div>
+          <div className = "boxPadding">
+            <div className = "box boxSize has-text-centered">
+              Employee Termination
+            </div>
+          </div>
+          <div className = "boxPadding">
+            <div className = "box boxSize has-text-centered">
+              Company Code of Conduct
             </div>
           </div>
         </div>
