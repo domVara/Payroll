3e153f365c51f11ba79619f0fa5d7d941f699fde
Dominic <dominic.varakukala@gmail.com>
2018-11-23 23:43:45 -0800

    fixes the add employee

--- models/employee.js
+++ models/employee.js
@@ -1,11 +1,14 @@
 let mongoose = require('mongoose')
 
 let employeeSchema = new mongoose.Schema({
-  firstName : String,
-  lastName : String,
-  position : String,
-  age : String,
-  team : String
+  firstName: String,
+  lastName: String,
+  team: String,
+  salary: String,
+  positionTitle: String,
+  email: String,
+  phoneNumber: String,
+  branchLocation: String,
 })
 
 module.exports = mongoose.model('Employee', employeeSchema)
