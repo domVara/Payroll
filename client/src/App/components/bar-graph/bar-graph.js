a3d6ab0bb800c1c0b3a686089e31ce42e810a7dc
Dominic <dominic.varakukala@gmail.com>
2018-12-02 11:17:58 -0800

    finished the folders part

--- client/src/App/components/bar-graph/bar-graph.js
+++ client/src/App/components/bar-graph/bar-graph.js
@@ -1,50 +1,67 @@
 import React, { Component } from "react";
 import {Bar} from 'react-chartjs-2';
+import axios from 'axios'
 import "./styles/bar-graph.css"
 
 
 class BarGraph extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      chartData: {
-        labels:['Boston','Worcester','Springfield','Lowell','Cambridge','New Bedford'],
-        datasets:[
-        {
-          label: 'Population',
-          data:[
-            617594,
-            181045,
-            153021,
-            106231,
-            105232,
-            95072
-          ],
-          backgroundColor:[
-            'rgba(255, 99, 132,  0.6)',
-            'rgba(255, 99, 132,  0.6)',
-            'rgba(255, 99, 132,  0.6)',
-            'rgba(255, 99, 132,  0.6)',
-            'rgba(255, 99, 132,  0.6)',
-            'rgba(255, 99, 132,  0.6)',
-          ]
-        }
-      ]
-      }
+      employees:[],
+      Data:{}
+    }}
+
+
+  async componentWillMount(){
+      await axios.get("/api/teams/" + this.props.val)
+        .then(res => {
+          console.log(res.data);
+          this.setState({employees : res.data})
+        })
+      var info = this.setGraph()
+      this.setState.Data = {
+        labels: info[0],
+              datasets:[
+                 {
+                    label:'Champions League 2017/2018 Top Scorer',
+                    data: info[1] ,
+                    backgroundColor:[
+                     'rgba(255,105,145,0.6)',
+                     'rgba(155,100,210,0.6)',
+                     'rgba(90,178,255,0.6)',
+                     'rgba(240,134,67,0.6)',
+                     'rgba(120,120,120,0.6)',
+                     'rgba(250,55,197,0.6)'
+                  ]
+                 }
+              ]
+           }
+    console.log(this.state.Data)
     }
+
+  setGraph(){
+    var _label = [];
+    var _datasets = [];
+    this.state.employees.forEach(employee => {
+      _label.push(employee.firstName)
+      _datasets.push(employee.hours)
+    })
+    return [_label, _datasets]
+
+
+
   }
 
   render() {
 
     return (
       <div>
-      <Bar
-        data = {this.state.chartData}
-      	options={{
-      		  maintainAspectRatio: true
-          }}
-      />
-
+        <Bar
+          data = {this.state.Data}
+          options = {{ maintainAspectRatio: false }}
+        />
       </div>
     );
   }
